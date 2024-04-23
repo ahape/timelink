26a7a4f550f9503be019800bf9f6356c3d596f66
Alan Hape <ahape@brightmetrics.com>
2024-04-22 21:47:24 -0700

    Run prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'timelink';
-  timeValue = "hello";
+  title = "timelink"
+  timeValue = "hello"
 
   change(event: Event) {
-    this.timeValue = (<HTMLInputElement>event.target).value;
+    this.timeValue = (<HTMLInputElement>event.target).value
   }
 }
