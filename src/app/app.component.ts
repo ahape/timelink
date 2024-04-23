7a63455ab9f86e14fd16f34d6bdfa0f204ce7b57
Alan Hape <ahape@brightmetrics.com>
2024-04-22 21:40:17 -0700

    something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'timelink';
+  timeValue = "hello";
+
+  change(event: Event) {
+    this.timeValue = (<HTMLInputElement>event.target).value;
+  }
 }
