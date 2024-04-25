7e963bde95c26f913f4fdf212658ea6eceba6c66
Alan Hape <ahape@brightmetrics.com>
2024-04-24 21:27:29 -0700

    it's fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from "@angular/common"
 import { Component, OnInit } from "@angular/core"
 import { RouterOutlet, ActivatedRoute, ParamMap } from "@angular/router"
 
-function f(timePart: number): string {
+function timePartToString(timePart: number): string {
   return timePart.toString().padStart(2, "0")
 }
 
@@ -31,26 +31,26 @@ export class AppComponent implements OnInit {
     this.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
     this.timeZoneNames = Intl.supportedValuesOf("timeZone")
 
-    this.route.queryParamMap.subscribe(queryParamMap => {
+    this.route.queryParamMap.subscribe((queryParamMap) => {
       this.tryLoadViaParams(queryParamMap)
     })
   }
 
   timeStringToDate(time: string): Date {
-    const parts = time.split(":").map(p => p.trim())
-    if (parts.length < 2 ||
-        parts.some(p => isNaN(parseInt(p, 10))))
-    {
+    const parts = time.split(":").map((p) => p.trim())
+    if (parts.length < 2 || parts.some((p) => isNaN(parseInt(p, 10)))) {
       return invalidDate
     }
-    if (parts.length == 2)
-      parts.push("0")
-    const [h, m, s] = parts.slice(0, 3).map(p => parseInt(p, 10))
+    if (parts.length == 2) parts.push("0")
+    const [h, m, s] = parts.slice(0, 3).map((p) => parseInt(p, 10))
     return new Date(seed.setHours(h, m, s))
   }
 
   formatTimeForRoundTrip(date: Date): string {
-    return `${f(date.getUTCHours())}:${f(date.getUTCMinutes())}:${f(date.getUTCSeconds())}`
+    const h = timePartToString(date.getUTCHours())
+    const m = timePartToString(date.getUTCMinutes())
+    const s = timePartToString(date.getUTCSeconds())
+    return `${h}:${m}:${s}`
   }
 
   formatTimeForDisplay(date: Date, timeZone: string): string {
@@ -63,8 +63,7 @@ export class AppComponent implements OnInit {
 
   createLink(): string {
     let hostname = location.hostname
-    if (hostname.includes("localhost"))
-      hostname += ":" + location.port
+    if (hostname.includes("localhost")) hostname += ":" + location.port
     const t = encodeURIComponent(this.formatTimeForRoundTrip(this.timeValue!))
     return `${location.protocol}//${hostname}?t=${t}`
   }
@@ -76,7 +75,7 @@ export class AppComponent implements OnInit {
   update(timeText: string) {
     this.timeText = timeText
 
-    const date = this.timeValue = this.timeStringToDate(timeText)
+    const date = (this.timeValue = this.timeStringToDate(timeText))
     if (date !== invalidDate) {
       this.timeTextDisplay = this.formatTimeForDisplay(date, this.timeZone)
       this.updateTimeZoneList(date)
@@ -88,30 +87,35 @@ export class AppComponent implements OnInit {
   tryLoadViaParams(paramMap: ParamMap) {
     const time = decodeURIComponent(paramMap.get("t") ?? "")
     if (time) {
-      const local = new Date(seed.toISOString().replace(/\d{2}:\d{2}:\d{2}/, time))
-      this.update(`${f(local.getHours())}:${f(local.getMinutes())}:${f(local.getSeconds())}`)
+      const local = new Date(
+        seed.toISOString().replace(/\d{2}:\d{2}:\d{2}/, time)
+      )
+      this.update(
+        `${timePartToString(local.getHours())}:${timePartToString(local.getMinutes())}:${timePartToString(local.getSeconds())}`
+      )
     }
   }
 
   updateTimeZoneList(timeValue: Date) {
-    const set = new Set<string>();
+    const set = new Set<string>()
     for (const timeZone of this.timeZoneNames)
       set.add(this.formatTimeForDisplay(timeValue, timeZone))
-    this.timeZoneList = Array.from(set).sort()
-    const localIndex = this.timeZoneList.indexOf(this.timeTextDisplay)
+    let arr = Array.from(set).sort()
+    const localIndex = arr.indexOf(this.timeTextDisplay)
     if (localIndex > -1)
-      this.timeZoneList = [
-        ...this.timeZoneList.slice(localIndex), // Make local time the top one
-        ...this.timeZoneList.slice(0, localIndex),
+      arr = [
+        ...arr.slice(localIndex), // Make local time the top one
+        ...arr.slice(0, localIndex),
       ]
+    this.timeZoneList = arr
   }
 
   async copyLinkToClipboard(text: string) {
     try {
-      const type = "text/plain";
-      const blob = new Blob([text], { type });
-      const data = [new ClipboardItem({ [type]: blob })];
-      await navigator.clipboard.write(data);
+      const type = "text/plain"
+      const blob = new Blob([text], { type })
+      const data = [new ClipboardItem({ [type]: blob })]
+      await navigator.clipboard.write(data)
     } catch (e) {
       document.execCommand("copy", false, text)
     }
